7296b2f59741a246408c64d764bf3245af8ffe8d
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2023-10-11 22:17:42 +0200

    Code inside test folder put in real folders. Still not working, maybe need to re start the project from zero ?!

--- p9_litreview/application_asgi.py
+++ p9_litreview/application_asgi.py
@@ -1,7 +1,11 @@
+# Importing the necessary module
 import os
 
+# Importing the get_asgi_application function from the django.core.asgi module
 from django.core.asgi import get_asgi_application
 
+# Setting the DJANGO_SETTINGS_MODULE environment variable to 'LITReview.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LITReview.settings')
 
+# Getting the ASGI application for the Django project
 application = get_asgi_application()
--- p9_litreview/url.py
+++ p9_litreview/url.py
@@ -1,22 +1,41 @@
+# Importing necessary modules and functions from Django
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Importing views and custom view for the users app
 from users import views as views
 from users.views import UnsubscribeView
 
+# URL patterns for the Django project
 urlpatterns = [
+    # Admin site URL
     path('admin/', admin.site.urls),
+
+    # URL for user registration
     path('register/', views.register, name='register'),
+
+    # URL for user profile page
     path('profile/', views.profile, name='profile'),
+
+    # URL for user login page using Django's built-in auth_views
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # URL for user logout page using Django's built-in auth_views
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    # URL for user subscriptions page
     path('subscriptions/', views.subscriptions, name='subscriptions'),
+
+    # URL for confirming unsubscribe action using custom view UnsubscribeView
     path('subscriptions/confirm_unsub/<int:pk>/', UnsubscribeView.as_view(), name='confirm-unsub'),
+
+    # Including URLs from the reviews app
     path('', include('reviews.urls')),
 ]
 
+# Adding static and media URL patterns if in DEBUG mode
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
